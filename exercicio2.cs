7760959522d55a1def9e9aab1b48eeaabd4394e9
Kikyo <lucaslideiro@gmail.com>
2026-05-09 18:12:22 -0300

    Create exercicio2.cs

---
+++ exercicio2.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("Digite seu peso:");
+double peso = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Digite sua altura:");
+double altura = double.Parse(Console.ReadLine()!);
+
+double imc = peso / (altura * altura);
+
+Console.WriteLine($"Seu IMC é {imc:F2}");
